6ba3bc40025e191a98401b7fafe905b2cd9d01ca
ryanscottjohnson <teacherman1@gmail.com>
2018-08-01 11:02:16 -0700

    added mongoose and bodyparser

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const express = require('express');
 const app = express();
 app.use(express.static('static'));
 
-console.log('hello world!')
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const mongoose = require('mongoose');
+// MongoClient.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+// // mongoose.connect('mongodb://localhost:3000', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI);
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+console.log('working');
 
 app.listen(process.env.PORT, () => {
     console.log(`listening on http://localhost:${process.env.PORT}`);
