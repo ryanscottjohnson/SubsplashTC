fe94f3f120d40ce99ab29d2ad9cc6d5113940a69
ryanscottjohnson <teacherman1@gmail.com>
2018-08-01 11:22:48 -0700

    basic structure of router working, needs refactor per API requirements

---
+++ routes/content.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const router = new express.Router();
+
+// TODO: add other dependencies for rendering content
+
+// get route (refine according to API requirements)
+router.get('/', (req, res) => {
+    if(req.query.id) {
+        // name Video needs to be changed according to API requirements
+        Video.findOne({_id: req.query.id}, (err, video) => {
+            res.send(video);
+        });
+    } else {
+        Video.find()
+        .then (video => {
+            res.send(video);
+        });
+    }
+});
+
+module.exports = router;
