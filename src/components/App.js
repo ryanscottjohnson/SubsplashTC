cc02284dc5bd8e6885a36f562b68751ea58c625b
ryanscottjohnson <teacherman1@gmail.com>
2018-08-05 20:12:08 -0700

    error at api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import VideoTable from './VideoTable'
-import appRequest from '../appReq'
+import VideoTable from './VideoTable.js';
+import mockData from '../appReq.js';
+// import appReq from '../appReq.js';
+
 class App extends React.Component {
-    constructor() {
-    super();
-    const results = appRequest.getVideos()
+    constructor(props) {
+    super(props);
+    // const results = appReq.getVideos();
+    const results = mockData.getVideo();
   }
   render(){
     return <VideoTable results={this.results} />
--- src/components/VideoTable.js
+++ src/components/VideoTable.js
@@ -3,21 +3,35 @@ import ReactDOM from 'react-dom';
 class VideoTable extends React.Component {
     constructor() {
     super();
+    this.renderVideo = this.renderVideo.bind(this);
   }
-renderVideo (data){
- return videos.map((item, index) =>{
+renderVideo (data) {
+ return data.map((item, index) =>{
   return <tr key={index}><td>{item.title}</td></tr>
  });
 }
+
   render(){
      const result = JSON.stringify(this.props.result)
      //this is where the table will go .. example
      return (
-     <table>
-       {this.renderVideos(result)}
-         <pre>{result}</pre>
-         </table>
+       <table>
+         <tbody>
+           {this.renderVideo(result)}
+           <tr>
+             <td>{result}</td>
+             <td>{id}</td>
+             <td>{title}</td>
+             <td>{youtube_url}</td>
+             <td>{published_at}</td>
+             <td>{reach}</td>
+
+
+
+           </tr>
+         </tbody>
+       </table>
      )
   }
-  }
+}
   export default VideoTable
